ed19dc69ea2b5922999069eb1188376e566615c3
Estpital <info@estpital.com>
2025-05-14 05:37:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import SacEkimi from './pages/SacEkimi';
 import Hizmetler from './pages/Hizmetler';
 import BurunEstetigi from './pages/BurunEstetigi';
+import Doktorlarimiz from './pages/Doktorlarimiz';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/sac-ekimi" element={<SacEkimi />} />
         <Route path="/hizmetler" element={<Hizmetler />} />
         <Route path="/burun-estetigi" element={<BurunEstetigi />} />
+        <Route path="/doktorlarimiz" element={<Doktorlarimiz />} />
       </Routes>
     </Router>
   );
