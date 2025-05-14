146dd5f5a0f7330bc89833fabb87dafddc614539
Estpital <info@estpital.com>
2025-05-14 03:58:04 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import SacEkimi from './pages/SacEkimi';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sac-ekimi" element={<SacEkimi />} />
       </Routes>
     </Router>
   );
