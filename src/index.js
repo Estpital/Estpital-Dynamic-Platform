438f549cf76c0f12b913febb8fe18f03f53e44c8
Estpital <info@estpital.com>
2025-05-14 05:23:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './i18n'; // i18next yapılandırmasını import et
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
