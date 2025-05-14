a1205655daa670eda5af93732934ea51bd522ad5
Estpital <info@estpital.com>
2025-05-14 21:50:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './i18n'; // i18next yapılandırmasını import et
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
