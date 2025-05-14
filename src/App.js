ee11bbf2fc3c1871e69a5561737e3d405ee3549c
Estpital <info@estpital.com>
2025-05-14 20:37:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import SacEkimi from './pages/SacEkimi';
-import Hizmetler from './pages/Hizmetler';
-import BurunEstetigi from './pages/BurunEstetigi';
-import Doktorlarimiz from './pages/Doktorlarimiz';
-import SacEkimiKampanya from './pages/SacEkimiKampanya';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sac-ekimi" element={<SacEkimi />} />
-        <Route path="/hizmetler" element={<Hizmetler />} />
-        <Route path="/burun-estetigi" element={<BurunEstetigi />} />
-        <Route path="/doktorlarimiz" element={<Doktorlarimiz />} />
-        <Route path="/sac-ekimi-kampanya" element={<SacEkimiKampanya />} />
       </Routes>
     </Router>
   );
