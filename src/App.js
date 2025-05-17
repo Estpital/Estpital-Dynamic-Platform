fe1f0e2b3111a72feed4c32975e4ecd1b34b0396
Estpital <info@estpital.com>
2025-05-18 01:14:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import Home from './pages/Home';
-import SacEkimi from './pages/SacEkimi';
-import Doktorlarimiz from './pages/Doktorlarimiz';
-import ErrorBoundary from './ErrorBoundary';
+import HairTransplant from './pages/HairTransplant'; // Yeni sayfa eklendi
+import Doctors from './pages/Doctors';
 
 function App() {
   return (
-    <ErrorBoundary>
+    <I18nextProvider i18n={i18n}>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sac-ekimi" element={<SacEkimi />} />
-          <Route path="/doktorlarimiz" element={<Doktorlarimiz />} />
+          <Route path="/sac-ekimi" element={<HairTransplant />} />
+          <Route path="/doktorlarimiz" element={<Doctors />} />
+          {/* Diğer hizmet sayfaları için yer tutucular */}
+          <Route path="/burun-estetigi" element={<div>Burun Estetiği Sayfası (Yer Tutucu)</div>} />
+          <Route path="/meme-estetigi" element={<div>Meme Estetiği Sayfası (Yer Tutucu)</div>} />
+          <Route path="/yuz-estetigi" element={<div>Yüz Estetiği Sayfası (Yer Tutucu)</div>} />
+          <Route path="/dolgu-botoks" element={<div>Dolgu ve Botoks Sayfası (Yer Tutucu)</div>} />
+          <Route path="/dis-tedavileri" element={<div>Diş Tedavileri Sayfası (Yer Tutucu)</div>} />
+          <Route path="/karin-germe" element={<div>Karın Germe Sayfası (Yer Tutucu)</div>} />
+          <Route path="/randevu" element={<div>Randevu Sayfası (Yer Tutucu)</div>} />
         </Routes>
       </Router>
-    </ErrorBoundary>
+    </I18nextProvider>
   );
 }
 
