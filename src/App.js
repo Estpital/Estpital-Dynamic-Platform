81338af166592bb84dfceacf267973652e0b30f9
Estpital <info@estpital.com>
2025-05-18 14:30:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import Home from './pages/Home';
 import SacEkimi from './pages/SacEkimi';
-import Doktorlarimiz from './pages/Doktorlarimiz'; // Doctors yerine Doktorlarimiz kullanıyoruz
+import Doktorlarimiz from './pages/Doktorlarimiz';
 import Shop from './pages/Shop';
+import Konsültasyon from './pages/Konsültasyon'; // Yeni sayfa eklendi
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/sac-ekimi" element={<SacEkimi />} />
           <Route path="/doktorlarimiz" element={<Doktorlarimiz />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/konsültasyon" element={<Konsültasyon />} /> {/* Yeni yol eklendi */}
           {/* Diğer hizmet sayfaları için yer tutucular */}
           <Route path="/burun-estetigi" element={<div>Burun Estetiği Sayfası (Yer Tutucu)</div>} />
           <Route path="/meme-estetigi" element={<div>Meme Estetiği Sayfası (Yer Tutucu)</div>} />
